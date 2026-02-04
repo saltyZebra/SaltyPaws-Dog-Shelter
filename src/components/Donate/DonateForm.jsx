97a3f9c3fe7eda7002766ae55dc5ea0560ae8698
malinhenning <malin.henningson@outlook.com>
2026-02-04 08:08:31 +0000

    Install react icons for icon  usage, change of styling to ourDogs page

--- src/components/Donate/DonateForm.jsx
+++ src/components/Donate/DonateForm.jsx
@@ -1,51 +1,56 @@
 import React from "react";
 import "./Donate.css";
+import { useState } from "react";
 
 function DonateForm() {
+  const [donate, setDonate] = useState("");
   // funtion som hindrar en omladdning av sidan vid klick på donate-knapp
-  function handleSubmit(event) {
-    event.preventDefault();
+  function handleSubmit(e) {
+    e.preventDefault();
     console.log("Donated");
-    // Hämtar form från nuvarande området som används, skapar en plats för Data som
+    // Hämtar form från nuvarande input som används, skapar en plats för Data som
     // skrivs in i form, och sen hämtar data och använder det någon annanstans
-    const formElement = event.currentTarget;
+    const formElement = e.currentTarget;
     const formData = new FormData(formElement);
     const name = formData.get("fullName");
     formElement.reset();
 
-    return <p>Thank you {name} for your donation!</p>;
+    setDonate(name);
   }
 
   return (
     <>
-      <form
-        onSubmit={handleSubmit}
-        action="fill"
-        className="donate-form"
-        method="post"
-      >
-        <input
-          type="name"
-          name="fullName"
-          className="donate-input-name"
-          placeholder="First- & lastname"
-        />
+      {donate === "" ? (
+        <form
+          onSubmit={handleSubmit}
+          action={handleSubmit}
+          className="donate-form"
+        >
+          <input
+            type="name"
+            name="fullName"
+            className="donate-input-name"
+            placeholder="First- & lastname"
+          />
 
-        <input
-          type="email"
-          name="email"
-          className="donate-input-info"
-          placeholder="Email"
-        />
+          <input
+            type="email"
+            name="email"
+            className="donate-input-info"
+            placeholder="Email"
+          />
 
-        <input
-          type="phone"
-          name="phone"
-          className="donate-input-info"
-          placeholder="Phone nr for swish"
-        />
-        <button className="donate-button">Donate</button>
-      </form>
+          <input
+            type="phone"
+            name="phone"
+            className="donate-input-info"
+            placeholder="Phone nr for swish"
+          />
+          <button className="donate-button">Donate</button>
+        </form>
+      ) : (
+        <p>Thank you {donate} for your donation!</p>
+      )}
     </>
   );
 }
--- src/components/OurDogs/DogsList.jsx
+++ src/components/OurDogs/DogsList.jsx
@@ -1,22 +1,38 @@
 import React from "react";
 import "./OurDogs.css";
+import { IoHeartOutline } from "react-icons/io5";
+import { IoHeartSharp } from "react-icons/io5";
+import { useState } from "react";
 
 function DogsList({ dogsArray }) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleClick = () => {
+    setIsFavorite((prevIsFavorite) => !prevIsFavorite);
+  };
+
   return (
     <>
       <ul className="dog-ul">
         {dogsArray.map((dog) => (
           <li key={dog.id} className="dog-li">
-            <img src={dog.image} className="dog-img" />
-            <h4>{dog.name}</h4>
-            <section className="dog-info">
-              <p>Age: {dog.age} years old</p>
-              <p>Breed: {dog.breed}</p>
+            <section className="dog-img">
+              <img src={dog.image} />
             </section>
-            <section className="dog-info">
-              <h4>About</h4>
-              <p>{dog.personality}</p>
-              <span className="material-symbols-outlined">favorite</span>
+            <section className="dog-info-section">
+              <h4>{dog.name}</h4>
+              <section className="dog-info">
+                <p>Age: {dog.age} years old</p>
+                <p>Breed: {dog.breed}</p>
+              </section>
+              <section className="dog-info">
+                <h4>About</h4>
+                <p>{dog.personality}</p>
+                <span className="material-symbols-outlined">favorite</span>
+              </section>
+              <p onClick={handleClick} style={{ cursor: "pointer" }}>
+                {isFavorite ? <IoHeartSharp /> : <IoHeartOutline />}
+              </p>
             </section>
           </li>
         ))}
